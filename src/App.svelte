78c614b254fe61b078d3c90df40a828da236b3c8
Rohan Fletcher <rohfle@gmail.com>
2021-03-21 19:24:49 +1300

    Basic reproduction of issue

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import './main.css';
+</script>
+
+<style>
+  .button {
+    @apply bg-blue-500 text-white inline-block p-2 rounded;
+  }
+</style>
+
+<div>This is styled using &lt;style&gt; and @apply</div>
+<div class="button">Button</div>
+
+<br>
+<br>
+<br>
+
+<div>This has the same styling as class attributes</div>
+<div class="bg-blue-500 text-white inline-block p-2 rounded">Button</div>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      width: {
+        '18': '4.5rem',
+        '68': '17rem'
+      },
+      minWidth: {
+        '48': '12rem'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/line-clamp'),
+  ],
+}
